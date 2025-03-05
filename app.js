87ea3df6ce046bb4bb758052ea27ec98abb9b8fd
LeeGisKer <ax_gil@hotmail.com>
2025-03-05 00:42:32 -0600

    update app.js

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres = [];
+let resultadosorteo = [];
+
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById('Amigo');
+    let nombre = inputAmigo.value.trim();
+
+    if (!nombre) {
+        alert('Debes ingresar un nombre');
+        return;
+    }
+
+    nombres.push(nombre);
+    inputAmigo.value = '';
+
+}
+
+function sortearAmigo(){
+
+    if (nombres.length === 0) {
+        alert('Debes ingresar al menos un amigo');
+        return;
+    }
+
+    let amigoAleatorio = Math.floor(Math.random() * nombres.length);
+    let amigoSorteado = nombres[amigoAleatorio];
+    let amigoResultado = document.getElementById('amigoResultado');
+    amigoResultado.innerHTML = amigoSorteado;
+
+}
+
+function limpiarCaja(){
+    
+
+}
